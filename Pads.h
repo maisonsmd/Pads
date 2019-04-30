04544b5b6b71503835f36d766d801aa584c23ca6
maisonsmd <maisonsmd@gmail.com>
2019-04-30 11:18:50 +0700

    Update Pads.h
    Fully support integer types, use stardard functions to copy strings intead of for loops

--- Pads.h
+++ Pads.h
@@ -1,84 +1,160 @@
-#pragma once
+#ifndef _PADS_H_
+#define _PADS_H_
+
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
 
 namespace Pads {
-	namespace {
-		constexpr uint8_t bufferSize = 15;
-		static char buffer[bufferSize];
+	namespace details {
+		constexpr uint8_t maxLength = 15;
+		static char buffer[maxLength + 1];
 	}
+
+	/*---------------------------------LEFT PADS----------------------------------*/
 
 	char * LeftPad(const char * text, uint8_t length, char placeHolder = ' ') {
 		if (placeHolder == 0)
 			placeHolder = ' ';
 
-		uint8_t i = 0;
 		uint8_t strLength = strlen(text);
+		// guard
+		if (length >= details::maxLength)
+			length = details::maxLength;
+		if (strLength >= details::maxLength)
+			strLength = details::maxLength;
 		if (length < strLength)
 			length = strLength;
 
-		while (i < length - strLength)
-			buffer[i++] = placeHolder;
+		memset(details::buffer, placeHolder, length - strLength);
+		strncpy(details::buffer + (length - strLength), text, strLength);
 
-		while (i < length) {
-			buffer[i] = text[i - (length - strLength)];
-			++i;
-		}
-		buffer[i] = '\0';
+		details::buffer[length] = '\0';
 
-		return buffer;
+		return details::buffer;
 	}
 
 	inline char * LeftPad(String & text, uint8_t length, char placeHolder = ' ') {
-		char strBuff[bufferSize];
-		text.toCharArray(strBuff, bufferSize);
+		char strBuff[details::maxLength];
+		text.toCharArray(strBuff, details::maxLength + 1);
 		return LeftPad(strBuff, length, placeHolder);
 	}
 
 	char * LeftPad(const double value, uint8_t length, uint8_t decimalPlaces = 2, char placeHolder = ' ') {
-		dtostrf(value, length, decimalPlaces, buffer);
+		dtostrf(value, length, decimalPlaces, details::buffer);
 		if (placeHolder == 0)
 			placeHolder = ' ';
+		if (length >= details::maxLength)
+			length = details::maxLength;
 
 		for (uint8_t i = 0; i < length; ++i)
-			if (buffer[i] == ' ')
-				buffer[i] = placeHolder;
+			if (details::buffer[i] == ' ')
+				details::buffer[i] = placeHolder;
 
-		return buffer;
+		return details::buffer;
 	}
+
+	inline char * LeftPad(const float value, uint8_t length, uint8_t decimalPlaces = 2, char placeHolder = ' ') {
+		return LeftPad((double)value, length, decimalPlaces, placeHolder);
+	}
+
+	inline char * LeftPad(const unsigned long value, uint8_t length, char placeHolder = ' ') {
+		char tempBuffer[details::maxLength + 1];
+		sprintf(tempBuffer, "%lu", value);
+		return LeftPad(tempBuffer, length, placeHolder);
+	}
+
+	inline char * LeftPad(const long value, uint8_t length, char placeHolder = ' ') {
+		char tempBuffer[details::maxLength + 1];
+		sprintf(tempBuffer, "%ld", value);
+		return LeftPad(tempBuffer, length, placeHolder);
+	}
+
+	inline char * LeftPad(const unsigned int value, uint8_t length, char placeHolder = ' ') {
+		return LeftPad((unsigned long)value, length, placeHolder);
+	}
+
+	inline char * LeftPad(const int value, uint8_t length, char placeHolder = ' ') {
+		return LeftPad((long)value, length, placeHolder);
+	}
+
+	/*---------------------------------RIGHT PADS----------------------------------*/
 
 	char * RightPad(const char * text, uint8_t length, char placeHolder = ' ') {
 		if (placeHolder == 0)
 			placeHolder = ' ';
 
-		uint8_t i = 0;
 		uint8_t strLength = strlen(text);
+		// guard
+		if (length >= details::maxLength)
+			length = details::maxLength;
+		if (strLength >= details::maxLength)
+			strLength = details::maxLength;
+		if (length < strLength)
+			length = strLength;
 
-		memcpy(buffer, text, strLength);
-		for (uint8_t i = strLength; i < length; ++i)
-			buffer[i] = placeHolder;
+		strncpy(details::buffer, text, strLength);
+		memset(details::buffer + strLength, placeHolder, length - strLength);
 
-		buffer[length] = '\0';
+		details::buffer[length] = '\0';
 
-		return buffer;
+		return details::buffer;
 	}
 
 	inline char * RightPad(String & text, uint8_t length, char placeHolder = ' ') {
-		char strBuff[bufferSize];
-		text.toCharArray(strBuff, bufferSize);
+		char strBuff[details::maxLength + 1];
+		text.toCharArray(strBuff, details::maxLength);
 		return RightPad(strBuff, length, placeHolder);
 	}
 
 	char * RightPad(const double value, uint8_t length, uint8_t decimalPlaces = 2, char placeHolder = ' ') {
-		dtostrf(value, 0, decimalPlaces, buffer);
 		if (placeHolder == 0)
 			placeHolder = ' ';
+		if (length >= details::maxLength)
+			length = details::maxLength;
 
-		uint8_t strLength = strlen(buffer);
+		char tempBuffer[details::maxLength + 1];
+		dtostrf(value, length, decimalPlaces, tempBuffer);
 
-		for (uint8_t i = strLength; i < length; ++i)
-			buffer[i] = placeHolder;
+		uint8_t startIndex = 0;
+		for (uint8_t i = 0; i < length; ++i)
+			if (tempBuffer[i] == ' ')
+				++startIndex;
+			else break;
 
-		buffer[length] = '\0';
+		strncpy(details::buffer, tempBuffer + startIndex, length - startIndex);
+		memset(details::buffer + (length - startIndex), placeHolder, startIndex);
+		
+		details::buffer[length] = '\0';
 
-		return buffer;
+		return details::buffer;
+	}
+
+	inline char * RightPad(const float value, uint8_t length, uint8_t decimalPlaces = 2, char placeHolder = ' ') {
+		return RightPad((double)value, length, decimalPlaces, placeHolder);
+	}
+
+	inline char * RightPad(const unsigned long value, uint8_t length, char placeHolder = ' ') {
+		char tempBuffer[details::maxLength + 1];
+		sprintf(tempBuffer, "%lu", value);
+		return RightPad(tempBuffer, length, placeHolder);
+	}
+
+	inline char * RightPad(const long value, uint8_t length, char placeHolder = ' ') {
+		char tempBuffer[details::maxLength + 1];
+		sprintf(tempBuffer, "%ld", value);
+		return RightPad(tempBuffer, length, placeHolder);
+	}
+
+	inline char * RightPad(const unsigned int value, uint8_t length, char placeHolder = ' ') {
+		return RightPad((unsigned long)value, length, placeHolder);
+	}
+
+	inline char * RightPad(const int value, uint8_t length, char placeHolder = ' ') {
+		return RightPad((long)value, length, placeHolder);
 	}
 }
+
+#endif
